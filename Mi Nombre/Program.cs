279ccd92350a56d3b283911ba8bb95d4a73351cc
RafaelB1307 <Rafael.bastides@uabc.edu.mx>
2022-05-21 01:29:55 -0700

    Agregar archivos de proyecto.

---
+++ Mi Nombre/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mi_Nombre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bastides Rafael");
+            Console.WriteLine("Mexicali, Baja California");
+            Console.WriteLine("");
+            Console.WriteLine("Programando en C#");
+            Console.ReadKey();
+
+        }
+    }
+}
